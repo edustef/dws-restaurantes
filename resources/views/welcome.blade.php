64dfa3c31eced5f0f330d49d71b02ce7ab8c5ce5
edustef <eduardstefan290@gmail.com>
2021-02-12 12:35:54 +0100

    updated logos and views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,23 +14,29 @@
 </head>
 
 <body class="antialiased">
-  <div class="flex w-screen min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-    <div class="fixed top-0 right-0 px-6 py-4 sm:block">
-      @auth
-        <a href="{{ route('dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
-      @else
-        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-        @if (Route::has('register'))
-        @endif
-      @endauth
+  <div class="flex min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <div class="fixed top-0 right-0 py-4 px-6 sm:block">
+      <div>
+        @auth
+          <a class="font-semibold text-white bg-red-400 shadow rounded-full px-4 py-2"
+            href="{{ route('dashboard') }}">Dashboard</a>
+        @else
+          <a class="font-semibold text-white bg-red-400 shadow rounded-full px-4 py-2" href="{{ route('login') }}">Log
+            in</a>
+          @if (Route::has('register'))
+          @endif
+        @endauth
+      </div>
     </div>
 
-    <div class="w-full max-w-6xl px-6 lg:px-8">
+    <div class="w-full max-w-6xl px-6">
       <div class="flex items-center space-x-2 text-3xl font-bold font-sans pt-8 sm:justify-start sm:pt-0">
       </div>
-      <div class="mt-8 dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+      <div class="mt-8 dark:bg-gray-800 overflow-hidden">
         @guest
-          <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Sign up</a>
+          <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Order now</a>
+        @else
+          <a href="{{ route('dashboard') }}" class="ml-4 text-sm text-gray-700 underline">Order now</a>
         @endguest
       </div>
     </div>

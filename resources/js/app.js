d10210d9b0de231783fbbbc9e4fa83f7ce772988
Eduard Stefan <eduardstefan290@gmail.com>
2021-03-06 22:45:07 +0100

    added js app starting point for inertia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,19 @@
-require('./bootstrap');
+import { InertiaApp } from "@inertiajs/inertia-react";
+import { InertiaProgress } from "@inertiajs/progress";
+import React from "react";
+import { render } from "react-dom";
 
-require('alpinejs');
+InertiaProgress.init();
+
+const app = document.getElementById("app");
+
+render(
+    <InertiaApp
+        initialPage={JSON.parse(app.dataset.page)}
+        resolveComponent={(name) =>
+            import(`./Pages/${name}`).then((module) => module.default)
+        }
+        resolveError={(page) => page.props.errors || {}}
+    />,
+    app
+);
